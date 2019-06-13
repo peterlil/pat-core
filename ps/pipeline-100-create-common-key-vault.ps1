2c62bb98361ac4026d4e48394e3dff605060a408
Peter Liljenroth <sail@live.se>
2019-06-13 13:01:28 +0200

    added the common key vault

--- ps/pipeline-100-create-common-key-vault.ps1
+++ ps/pipeline-100-create-common-key-vault.ps1
@@ -3,6 +3,10 @@ param(
     $sourcePath,
     $WhatIf
 )
+###############################################################################
+# Set verbose
+###############################################################################
+$VerbosePreference = "continue"
 
 ###############################################################################
 # Dump the parameters
@@ -16,6 +20,7 @@ Write-Verbose "sourcePath: $sourcePath"
 # Create the common properties key vault
 ###############################################################################
 
+Write-Verbose "Creating DevOps common properties key vault."
 #hard-coded
 $kvName = "pat-devops-kv"
 $rgName = "pat-core"
@@ -30,16 +35,10 @@ if($kv) {
     $kv = New-AzureRmKeyVault -VaultName $kvName -ResourceGroupName $rgName -Location $location -EnabledForDeployment -EnabledForTemplateDeployment -EnabledForDiskEncryption
 }
 
-
-###############################################################################
-# Add the Tenant ID
-###############################################################################
-$context = Get-AzureRmContext
-Set-AzureKeyVaultSecret -VaultName $kvName -Name $"TenantId" -SecretValue (ConvertTo-SecureString $context.Tenant.Id -AsPlainText -Force)
-
 ###############################################################################
 # Add the object id of the DevOps account
 ###############################################################################
+Write-Verbose "Getting the DevOps account Object Id"
 $azDevOpsAppName = "pat-devops" #hard-coded
 $azureAdApplication = Get-AzureRmADApplication -DisplayNameStartWith $azDevOpsAppName -ErrorAction SilentlyContinue
 if( $azureAdApplication ) {
@@ -49,11 +48,21 @@ if( $azureAdApplication ) {
 	exit 1
 }
 
+Write-Verbose "Setting access policies"
 Set-AzureRmKeyVaultAccessPolicy -VaultName $kvName -ObjectId $azureAdApplication -EnabledForDeployment -EnabledForTemplateDeployment `
     -PermissionsToKeys Decrypt,Encrypt,UnwrapKey,WrapKey,Verify,Sign,Get,List,Update,Create,Import,Delete,Backup,Restore,Recover,Purge `
     -PermissionsToSecrets Get,List,Set,Delete,Backup,Restore,Recover,Purge `
     -PermissionsToCertificates Get,List,Delete,Create,Import,Update,Managecontacts,Getissuers,Listissuers,Setissuers,Deleteissuers,Manageissuers,Recover,Backup,Restore,Purge `
     -PassThru
 
+Write-Verbose "Adding the DevOpsAccountObjectId to key vault"
 #hard-coded
 Set-AzureKeyVaultSecret -VaultName $kvName -Name "DevOpsAccountObjectId" -SecretValue (ConvertTo-SecureString $azureAdApplication.ApplicationId -AsPlainText -Force)
+
+###############################################################################
+# Add the Tenant ID
+###############################################################################
+Write-Verbose "Adding TenantId to key vault"
+$context = Get-AzureRmContext
+Set-AzureKeyVaultSecret -VaultName $kvName -Name "TenantId" -SecretValue (ConvertTo-SecureString $context.Tenant.Id -AsPlainText -Force)
+
